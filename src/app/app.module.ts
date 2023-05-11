7f8fb65a0907d7bf71d9905986fa7e90d3439513
Khushi <Khushirana1101@gmail.com>
2023-05-11 22:11:54 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { IfCompareComponent } from './if-compare/if-compare.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IfCompareComponent
   ],
   imports: [
     BrowserModule
